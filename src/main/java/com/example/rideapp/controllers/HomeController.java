5fe8281c45c1bfbbda0bd765fe43dbb83577bec5
Banan377 <ban484479@gmail.com>
2025-12-01 08:22:39 +0300

    prepare the link

---
+++ src/main/java/com/example/rideapp/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package main.java.com.example.rideapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/signup.html"; 
+    }
+}
